898dc1d9e7973b4d81e493c13d43f28c81a56917
Crollie Rollz <crollierollz@gmail.com>
2022-02-28 13:03:51 +0800

    upd conf

--- public/src/config.ts
+++ public/src/config.ts
@@ -1,6 +1,6 @@
 export default {
-    REDIS_URL: `redis://default:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    REDIS_URL: `redis://default:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT ?? 6379}`,
     MYSQL_HOST: process.env.DB_HOST,
-    MYSQL_PORT: process.env.DB_PORT,
+    MYSQL_PORT: process.env.DB_PORT ?? 3306,
     MYSQL_PASS: process.env.DB_PASS,
 }
