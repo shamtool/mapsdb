60a186f10892707bf3064f5ebcb28159eda18295
Crollie Rollz <crollierollz@gmail.com>
2022-03-03 02:50:55 +0800

    upd sql

--- db/sql-init/01-setup.sql
+++ db/sql-init/01-setup.sql
@@ -37,19 +37,19 @@ ALTER TABLE `CategoryAttributes`
 CREATE DATABASE IF NOT EXISTS `mapsdb` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
 USE `mapsdb`;
 
-CREATE TABLE `AttributeValues` (
+CREATE TABLE `MapAttributes` (
+  `mapCode` int(11) NOT NULL,
+  `attrId` varchar(127) NOT NULL,
+  `attrValueId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `MapAttributeValue` (
   `id` int(11) NOT NULL,
   `integerValue` int(11) DEFAULT NULL,
   `doubleValue` double DEFAULT NULL,
   `stringValue` varchar(2047) DEFAULT NULL,
   `booleanValue` tinyint(1) DEFAULT NULL,
   `longStringValue` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-CREATE TABLE `MapAttributes` (
-  `mapCode` int(11) NOT NULL,
-  `attrId` varchar(127) NOT NULL,
-  `attrValueId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `MapCategories` (
@@ -63,13 +63,13 @@ CREATE TABLE `MapEntry` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 
-ALTER TABLE `AttributeValues`
-  ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `MapAttributes`
   ADD PRIMARY KEY (`mapCode`,`attrId`);
 
+ALTER TABLE `MapAttributeValue`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `AttributeValues`
+
+ALTER TABLE `MapAttributeValue`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
